12b7c9b140dd251f230535ea43d5f8cb320545ef
Eric <Eric>
2023-02-07 23:18:27 -0500

    Add Search Navigation Links

---
+++ Dictonary_V1b/View/SearchAnnounc.swift
@@ -0,0 +1,20 @@
+//
+//  SearchAnnounc.swift
+//  Dictonary_V1b
+//
+//  Created by Eric Sousa on 2/7/23.
+//
+
+import SwiftUI
+
+struct SearchAnnounc: View {
+    var body: some View {
+        Text("Search Announcements")
+    }
+}
+
+struct SearchAnnounc_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchAnnounc()
+    }
+}
--- Dictonary_V1b/View/SearchView.swift
+++ Dictonary_V1b/View/SearchView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct SearchView: View {
+  //  var tipos = ["Search by Word", "Search by Category", "Announcements", "Dialogues"]
     var body: some View {
-        VStack(alignment: .center, spacing:0){
-                    Text("Search") .font(.title).bold()
-               Spacer()
+        VStack(alignment: .leading){
+            Text("Search").font(.title).bold().padding(.bottom, 20.0)
+            NavigationView {
+                ScrollView{
+            VStack(alignment: .leading, spacing: 25.0){ NavigationLink( destination: SearchWord(), label: { Text("Search by Word").foregroundColor(.white).frame(minWidth: 0, maxWidth: 200)
+                    .padding(.all,20).font(.title2)
+                    .foregroundColor(.white)
+                    .background(LinearGradient(gradient: Gradient(colors: [.blue, .blue]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(10)})
+                NavigationLink( destination: SearchCategory(), label: { Text("Search by Category").foregroundColor(.white).frame(minWidth: 0, maxWidth: 200)
+                        .padding(.all,20).font(.title2)
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [.blue, .blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(10)})
+                NavigationLink( destination: SearchAnnounc(), label: { Text("Announcements").foregroundColor(.white).frame(minWidth: 0, maxWidth: 200)
+                        .padding(.all,20).font(.title2)
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [.blue, .blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(10)})
+                NavigationLink( destination: SearchDialoge(), label: { Text("Dialogues").foregroundColor(.white).frame(minWidth: 0, maxWidth: 200)
+                        .padding(.all,20).font(.title2)
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [.blue, .blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(10)})
+            }
+            .padding(.bottom)
+                }}}
+        .padding(.horizontal)
+        }}
+         /*      Spacer()
                    VStack(alignment: .center, spacing:25){
                    Button("Search by Word"){}.foregroundColor(.white).frame(minWidth: 0, maxWidth: 200)
                            .padding(.all,20).font(.title2)
@@ -38,10 +66,8 @@ struct SearchView: View {
                Spacer()
                }
                .padding(.all)
-              
-           }
-       }
-
+              */
+       
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
